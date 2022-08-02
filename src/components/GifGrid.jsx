51af6a3dbde1da87de5a3e7f96b40b1257236f26
Tomas <tomasgp1997@gmail.com>
2022-08-02 19:04:22 -0300

    108

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from "react";
 import { GifItem } from "./GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
+import PropTypes from "prop-types";
 // import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ( {category} ) => {
@@ -45,4 +46,9 @@ export const GifGrid = ( {category} ) => {
 
     </>
   )
+}
+
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired,
 }
